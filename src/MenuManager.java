25749c1afff0fd007dacb870259957b06b4a18c4
FenderRoy <visser.roy@mail.com>
2022-03-14 15:10:58 +0100

    Code Comments

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -12,7 +12,11 @@ public class MenuManager {
 
     private Scanner scanner = new Scanner(System.in);
 
+
+    //Arraylist houd de huidige menu opties bij
     private ArrayList<iMenuOption> menuOptions;
+
+    //Arraylist die houd alle vorige menuopties bij voor backtracking
     private List<ArrayList<iMenuOption>> previousMenu = new ArrayList<ArrayList<iMenuOption>>();
 
     public void start(){
@@ -29,41 +33,51 @@ public class MenuManager {
     }
 
     public void reCheckUserInput(){
+        printMenu();
         processUserInput(getUserInput());
     }
 
     private void processUserInput(int index){
 
-
+        //Als de 0: exit menu is gekozen
         if(index == 0){
-            //We're in the main menu
+            //Kijken of we in de main menu zitten en zowel gwn weg gaan
             if(previousMenu.size() == 0){
                 return;
             }
 
+            //De huidige menu options terug zetten naar de vorige menu opties
             menuOptions = previousMenu.get(previousMenu.size() -1);
+            //de vorige menu opties die zijn gecloned verwijderen omdat we ze niet meer nodig hebben
             previousMenu.remove(previousMenu.size()-1);
 
             print();
             return;
         }
 
+        //Een array begint met 0, maar de menu opties beginnen met 1, dus we willen de index met 1 verminderen
         index--;
+
+        //Voer de code uit van de gekozen menu en sla de volgende submenu op in een variable
         menuOptions.get(index).executeMenuOption();
         var newMenuOptions = menuOptions.get(index).getNextSubMenu();
 
+        //Als er geen volgende menu opties zijn checken we voor nieuwe input
         if(newMenuOptions == null){
             reCheckUserInput();
             return;
         }
 
+        //het huidige menu opslaan voor backtracking
         previousMenu.add(menuOptions);
 
+        //het huidige menu updaten met de nieuwe submenu
         menuOptions = newMenuOptions;
         print();
     }
 
     private int getUserInput(){
+
         boolean noValidAnswer = true;
         while(noValidAnswer) {
             try {
@@ -73,6 +87,7 @@ public class MenuManager {
                 if (answer > menuOptions.size() || answer < 0) {
                     System.out.println("Please enter a valid number.");
                 } else {
+                    //Return statement haalt ons uit de while loop
                     return answer;
                 }
             } catch (InputMismatchException e) {
@@ -80,11 +95,14 @@ public class MenuManager {
                 System.out.println("Please enter a valid number.");
             }
         }
+
+        //Mandatory return statement, actually does nothing
         return 0;
     }
 
     private void printMenu(){
 
+        //Ga langs elke submenu optie en print de naam
         for(int i = 0; i < menuOptions.size(); i++){
             System.out.println(i+1 + ": "+menuOptions.get(i).getTitle());
         }
@@ -92,6 +110,8 @@ public class MenuManager {
     }
 
     private ArrayList<iMenuOption> getDefaultMenuOptions(){
+
+        //Dit is het startmenu
         ArrayList<iMenuOption> menuOptions = new ArrayList<>();
         menuOptions.add(new DierenMenuOption());
         menuOptions.add(new ExamenMenuOption());
